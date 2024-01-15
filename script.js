402a442938105b30c8b96a660f65063046e13738
MohamedKoujil <mohamed.koujil.7e7@itb.cat>
2024-01-15 10:46:02 +0100

    he hecho un par de funciones

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var aciertos = 0;
 
 function init() {
     //1. Genera el código random del master
-
+    generaCombinacion();
     //2. Crea todas las filas según el número de intentos.
 }
 
@@ -25,13 +25,39 @@ function init() {
 introducido el usuario.
 Informamos al usuario del resultado y del número de intentos que lleva*/
 function Comprobar() {
+    for (var i = 0; i < master.length; i++) {
+        if (master[i] == userCombi[i]) {
+            aciertos++;
+        } else intento++;
+    }
+    console.log("Aciertos: " + aciertos);
+    console.log("Intentos: " + intento);
 }
 
 /** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
 function añadeColor(color) {
-   
+    if (userCombi.length < MAX_COMBI_COLORES) {
+        userCombi.push(color);
+        console.log(userCombi);
+    } else {
+        alert("No puedes añadir más colores");
+    }
 }
 
+//Genera un número aleatorio entre 0 y el número de colores disponibles.
+function generaNum() {
+    var random = Math.floor(Math.random() * COLORS.length);
+    console.log("Random: " + random);
+    return COLORS[random];
+}
+
+/** Genera la combinación de colores que el usuario debe adivinar. */
+function generaCombinacion() {
+    for (var i = 0; i < MAX_COMBI_COLORES; i++) {
+        master.push(generaNum());
+    }
+    console.log(master);
+}
 
 /** Template con el código HTML que corresponde a cada fila de juego/intento. */
 const ROW_RESULT = `<div class="rowResult w100 flex wrap">
