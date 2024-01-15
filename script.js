d949e8315ea947a0bc626e62374fbedd44741197
MohamedKoujil <mohamed.koujil.7e7@itb.cat>
2024-01-15 10:21:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+//Declaración de constantes.
+const MAX_INTENTOS = 10;
+const MAX_COMBI_COLORES = 4;
+const COLORS = ['white', 'blue', 'green', 'violet', 'yellow', 'red', 'orange', 'cyan'];
+const GREY = "grey";
+const WHITE = "white";
+const BLACK = "black";
+
+
+//Declaración de variables globales.
+const master = [];
+const userCombi = [];
+var intento = 0;
+var aciertos = 0;
+
+function init() {
+    //1. Genera el código random del master
+
+    //2. Crea todas las filas según el número de intentos.
+}
+
+
+
+/* Llamaremos a esta función desde el botón HTML de la página para comprobar la propuesta de combinación que nos ha
+introducido el usuario.
+Informamos al usuario del resultado y del número de intentos que lleva*/
+function Comprobar() {
+}
+
+/** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
+function añadeColor(color) {
+   
+}
+
+
+/** Template con el código HTML que corresponde a cada fila de juego/intento. */
+const ROW_RESULT = `<div class="rowResult w100 flex wrap">
+    <div class="rowUserCombi w75 flex wrap">
+       <div class="w25">
+           <div class="celUserCombi flex"></div>
+       </div>
+       <div class="w25">
+           <div class="celUserCombi flex"></div>
+       </div>
+       <div class="w25">
+           <div class="celUserCombi flex"></div>
+       </div>
+       <div class="w25">
+           <div class="celUserCombi flex"></div>
+       </div>
+    </div>alis/Mastermind_CODIGO
+    <div class="rowCercleResult w25 flex wrap center">
+       <div class="w40 h40">
+            <div class="cercleResult flex"></div>
+       </div>
+       <div class="w40 h40">
+           <div class="cercleResult flex"></div>
+       </div>
+       <div class="w40 h40">
+           <div class="cercleResult flex"></div>
+       </div>
+       <div class="w40 h40">
+           <div class="cercleResult flex"></div>
+       </div>
+    <div>
+</div>`;
